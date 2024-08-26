e5468d1681b2a84eddc36a727c046408293e6283
Kaan Uçarcı <info@kaanucarci.com>
2024-08-27 00:41:46 +0300

    Created user table in the database. Defined two roles: Admin and User. Implemented login/register services based on roles. Developed separate dashboards for Admin and User.

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1>User Dashboard</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware(['auth'])->group(function (){
+   Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+
+Route::middleware(['auth', AuthAdmin::class])->group(function (){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
